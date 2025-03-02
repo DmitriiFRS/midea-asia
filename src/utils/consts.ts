55bf40d9397343e4e28d278f1567520153bea6f7
Dmitriy <dmitriifrs@gmail.com>
2025-03-02 17:21:44 +0500

    fix

--- src/utils/consts.ts
+++ src/utils/consts.ts
@@ -1,2 +1,5 @@
-export const strapiUrl = "http://localhost:1337";
+// export const strapiUrl = "http://localhost:1337";
+// export const strapiApiUrl = `${strapiUrl}/api`;
+
+export const strapiUrl = "https://admin.climate-academy.uz";
 export const strapiApiUrl = `${strapiUrl}/api`;
