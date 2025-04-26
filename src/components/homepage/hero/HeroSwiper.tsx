1b94f53e93ef7c70fb71a614ebaba2289419de3f
DmitriiFRS <dmitriifrs@gmail.com>
2025-04-26 12:08:37 +0500

    upd

--- src/components/homepage/hero/HeroSwiper.tsx
+++ src/components/homepage/hero/HeroSwiper.tsx
@@ -7,29 +7,28 @@ import "swiper/css/autoplay";
 import img from "@img/home/temp.jpeg";
 import slide1 from "@img/home/card1.webp";
 import slide2 from "@img/home/card2.webp";
-import slide3 from "@img/home/card3.webp";
 import Image from "next/image";
 import { Autoplay, EffectFade } from "swiper/modules";
 
 interface Props {
-     locale: string;
+  locale: string;
 }
 
 const HeroSwiper: React.FC<Props> = ({ locale }) => {
-     const slides = [slide1, slide2, slide3];
-     return (
-          <div className="absolute top-0 left-0 w-full h-screen -z-20">
-               <Swiper modules={[Autoplay, EffectFade]} autoplay={{ delay: 3000 }} loop={true} speed={2000} effect="fade">
-                    {slides.map((slide, index) => (
-                         <SwiperSlide key={index}>
-                              <div className="w-full h-full">
-                                   <Image src={slide} alt="slide" width={1980} height={1020} className="w-full h-screen object-cover" />
-                              </div>
-                         </SwiperSlide>
-                    ))}
-               </Swiper>
-          </div>
-     );
+  const slides = [slide1, slide2];
+  return (
+    <div className="absolute top-0 left-0 w-full h-screen -z-20">
+      <Swiper modules={[Autoplay, EffectFade]} autoplay={{ delay: 3000 }} loop={true} speed={2000} effect="fade">
+        {slides.map((slide, index) => (
+          <SwiperSlide key={index}>
+            <div className="w-full h-full">
+              <Image src={slide} alt="slide" width={1980} height={1020} className="w-full h-screen object-cover" />
+            </div>
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </div>
+  );
 };
 
 export default HeroSwiper;
